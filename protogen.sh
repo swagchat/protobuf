10f9304d33953c938ef1eca0e1e61dc6e6b06d71
betchi <shinichi.minobe@gmail.com>
2018-07-15 15:11:28 +0900

    update

--- protogen.sh
+++ protogen.sh
@@ -2,12 +2,23 @@
 
 rm -f ./*.gw.go
 rm -f ./*.pb.go
+rm -f ./*.gen.go
 
 protoc \
   -I./ \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --gofast_out=plugins=grpc:../../../ \
+  -I$GOPATH/src/github.com/gogo/protobuf \
+  --gogo_out=plugins=grpc:../../../ \
   --grpc-gateway_out=logtostderr=true:../../../ \
   chatMessage.proto \
-  chatService.proto
+  chatService.proto \
+  deviceMessage.proto \
+  commonMessage.proto \
+  roomMessage.proto \
+  roomUserMessage.proto \
+  roomUserService.proto \
+  userMessage.proto \
+  userService.proto \
+  userRoleMessage.proto \
+  userRoleService.proto
 
